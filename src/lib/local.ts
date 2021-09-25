b7627f76a9352d8141c86b2aa2563d6df00a34b1
UwwUzzz <85221145+UwwUzzz@users.noreply.github.com>
2021-09-26 01:25:57 +0700

    Update local.ts

--- src/lib/local.ts
+++ src/lib/local.ts
@@ -1,7 +1,7 @@
 import { writable } from 'svelte/store'
 import { browser } from '$app/env'
 
-export const count = writable(+((browser && localStorage.getItem('count')) || '0'));
+export const count = writable(+((browser && localStorage.getItem('count')) || '99999999999999'));
 
 count.subscribe((val) => {
   if (browser) return (localStorage.count = `${val}`);
